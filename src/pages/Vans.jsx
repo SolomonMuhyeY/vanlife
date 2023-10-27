7b8b585eea12c92a3d336586c863f935e9d4f397
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-27 11:42:11 +0300

    useing searchParams

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,16 @@
 import "../styles/vans.css";
 import { vans } from "../data/vans_data";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Vans() {
-  let van_card = vans.map((van) => {
+  const [searchParams, setSearchParams] = useSearchParams("");
+  const typeFilter = searchParams.get("type");
+
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type?.toLowerCase() === typeFilter)
+    : vans;
+  console.log(typeFilter);
+  let van_card = displayedVans.map((van) => {
     return (
       <div className='van_card' key={van.id}>
         <Link to={`/vans/${van.id}`}>
@@ -23,14 +30,34 @@ function Vans() {
     <div className='vans'>
       <div className='header'>
         <h2>Explore Our Van options</h2>
-        <nav>
-          <ul>
-            <li>Modern</li>
-            <li>Classic</li>
-            <li>Normal</li>
-            <li>Other News</li>
-          </ul>
-        </nav>
+        <div className='filtering_btns'>
+          <button
+            className='luxury_filter'
+            onClick={() => setSearchParams({ type: "luxury" })}
+          >
+            Luxury
+          </button>
+          <button
+            className='exotic_filter'
+            onClick={() => setSearchParams({ type: "exotic" })}
+          >
+            Exotic
+          </button>
+          <button
+            className='leisure_filter'
+            onClick={() => setSearchParams({ type: "leisure" })}
+          >
+            Leisure
+          </button>
+          {typeFilter ? (
+            <button
+              className='clear_filter'
+              onClick={() => setSearchParams({ type: "" })}
+            >
+              Clear Filters
+            </button>
+          ) : null}
+        </div>
       </div>
       <div className='van_card_container'>{van_card}</div>
     </div>
