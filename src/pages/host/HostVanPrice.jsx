2e77f2f52e8c0669887c0342789ffb2936e14e01
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-26 09:10:25 +0300

    changing the layout

--- src/pages/host/HostVanPrice.jsx
+++ src/pages/host/HostVanPrice.jsx
@@ -3,9 +3,9 @@ import { useOutletContext } from "react-router-dom";
 function HostVanPrice() {
   const { van } = useOutletContext();
   return (
-    <div className='mt-8 px-4 text-center'>
+    <div className='mt-8 px-4'>
       <h2 className='text-xl font-medium mb-2'>Price per Day</h2>
-      <h3 className='text-3xl font-semibold text-green-600'>${van.price}</h3>
+      <h3 className='text-xl font-semibold text-green-600'>{van.price}</h3>
     </div>
   );
 }
