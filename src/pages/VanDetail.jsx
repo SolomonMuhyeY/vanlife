5fcc77ea52ab0435975837f8e1fa867e319aa948
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-26 09:16:41 +0300

    adding review section

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -4,45 +4,22 @@ import { vans } from "../data/vans_data";
 function VanDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
+
   const goBack = () => {
     navigate(-1);
   };
-  let type = ""; //trying to avoid the global variable
-  const vanDetail = vans.map((van) => {
-    if (van.id == id) {
-      type = van.type;
-      return (
-        <div className='van_card_detail' key={van.id}>
-          <div className='img'>
-            <img src={van.img} alt='' />
-          </div>
-          {van.type && (
-            <button className={`type_btn ${van.type}`}>{van.type}</button>
-          )}
-          <b>{van.name}</b>
-          <small
-            style={{
-              margin: "8px 0",
-              display: "block",
-            }}
-          >
-            <b>{van.price}</b>
-          </small>
-          <p>{van.description}</p>
-          <button className='rent_btn'>rent this van</button>
-        </div>
-      );
-    }
-  });
+
+  const van = vans.find((van) => van.id == id);
+
+  if (!van) {
+    return <h1>Loading...</h1>;
+  }
+
+  const { type, img, name, price, description } = van;
 
   return (
-    <div className='detail_container'>
-      <NavLink
-        onClick={() => goBack()}
-        to='..'
-        relative='path'
-        className='rtn_btn'
-      >
+    <div className='detail_container p-4'>
+      <NavLink onClick={goBack} to='..' relative='path' className='rtn_btn'>
         &larr;
         <span style={{ marginRight: "0" }}>
           back to{" "}
@@ -55,9 +32,26 @@ function VanDetail() {
             {type ? type : "all"}
           </i>{" "}
           vans
-        </span>{" "}
+        </span>
       </NavLink>
-      {vanDetail ? vanDetail : <h1>Loading ... </h1>}
+      <div className='van_card_detail mt-6 p-6 border rounded-lg shadow-lg'>
+        <div className='img mb-4'>
+          <img src={img} alt={name} className='w-full h-auto rounded-lg' />
+        </div>
+        {type && (
+          <button className={`type_btn ${type.toLowerCase()} mb-4`}>
+            {type}
+          </button>
+        )}
+        <h2 className='text-2xl font-bold mb-2'>{name}</h2>
+        <div className='text-xl text-gray-700 mb-4'>
+          <b>${price}</b> / day
+        </div>
+        <p className='text-gray-600 mb-6'>{description}</p>
+        <button className='rent_btn bg-blue-500 text-white py-2 px-4 rounded'>
+          Rent this van
+        </button>
+      </div>
     </div>
   );
 }
