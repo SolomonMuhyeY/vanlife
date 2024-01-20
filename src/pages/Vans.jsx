4073514b629d851fff18f0496afbe4b614733e98
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-20 23:02:49 +0300

    changing the styling into tailwind

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -11,23 +11,24 @@ function Vans() {
 
   const vanCards = displayedVans.map((van) => (
     <div
-      className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4'
+      className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8'
       key={van.id}
     >
-      <Link to={`${van.id}`} className='block relative group overflow-hidden'>
+      <Link
+        to={`${van.id}`}
+        className='block relative overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition duration-300'
+      >
         <img
           src={van.img}
           alt='van image'
-          className='w-full h-auto object-cover'
+          className='w-full h-56 object-cover rounded-t-lg'
         />
-        <div className='absolute bottom-0 left-0 right-0 p-4 bg-black bg-opacity-75 text-white group-hover:bg-opacity-0 transition-opacity duration-300'>
-          <p className='text-lg font-semibold group-hover:text-white'>
-            {van.name}
-          </p>
-          <p className='text-sm group-hover:text-white'>{van.price}</p>
+        <div className='p-4 bg-white rounded-b-lg'>
+          <p className='text-lg font-semibold text-gray-800'>{van.name}</p>
+          <p className='text-sm text-gray-600 mt-1'>{van.price}</p>
           {van.type && (
             <button
-              className={`mt-2 px-3 py-1 bg-${van.type.toLowerCase()}-500 text-white rounded-full group-hover:bg-white group-hover:text-black transition-colors duration-300`}
+              className={`mt-2 px-3 py-1 bg-${van.type.toLowerCase()}-500 text-white rounded-full`}
             >
               {van.type}
             </button>
@@ -38,12 +39,12 @@ function Vans() {
   ));
 
   return (
-    <div className='bg-gray-100'>
-      <div className='container mx-auto py-8'>
-        <div className='text-center mb-8'>
-          <h2 className='text-3xl font-semibold'>Explore Our Van Options</h2>
-        </div>
-        <div className='flex flex-wrap mb-8'>
+    <div className='bg-gray-100 py-12'>
+      <div className='container mx-auto'>
+        <h2 className='text-3xl font-semibold text-center mb-8'>
+          Explore Our Van Options
+        </h2>
+        <div className='flex justify-center mb-8'>
           <button
             className={`mr-4 mb-4 px-4 py-2 bg-blue-500 text-white rounded ${
               typeFilter === "luxury" ? "bg-blue-700" : ""
