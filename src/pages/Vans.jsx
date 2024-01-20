d5bd7d90bca740872840c5e632a422cc57163126
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-20 23:00:49 +0300

    changing the styling into tailwind

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -14,14 +14,20 @@ function Vans() {
       className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4'
       key={van.id}
     >
-      <Link to={`${van.id}`} className='block relative overflow-hidden'>
-        <img src={van.img} alt='van image' className='w-full h-auto' />
-        <div className='absolute bottom-0 left-0 right-0 p-4 bg-black bg-opacity-75 text-white'>
-          <p className='text-lg font-semibold'>{van.name}</p>
-          <p className='text-sm'>{van.price}</p>
+      <Link to={`${van.id}`} className='block relative group overflow-hidden'>
+        <img
+          src={van.img}
+          alt='van image'
+          className='w-full h-auto object-cover'
+        />
+        <div className='absolute bottom-0 left-0 right-0 p-4 bg-black bg-opacity-75 text-white group-hover:bg-opacity-0 transition-opacity duration-300'>
+          <p className='text-lg font-semibold group-hover:text-white'>
+            {van.name}
+          </p>
+          <p className='text-sm group-hover:text-white'>{van.price}</p>
           {van.type && (
             <button
-              className={`mt-2 px-3 py-1 bg-${van.type.toLowerCase()}-500 text-white rounded-full`}
+              className={`mt-2 px-3 py-1 bg-${van.type.toLowerCase()}-500 text-white rounded-full group-hover:bg-white group-hover:text-black transition-colors duration-300`}
             >
               {van.type}
             </button>
