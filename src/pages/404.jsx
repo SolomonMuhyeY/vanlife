5c4d6343f06c013ef3374eac6e2f7122c6a1f955
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-20 22:44:26 +0300

    modfying the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import "../styles/styles.css"; // Import your CSS file
 
 function NotFound() {
   return (
-    <div className='not_found'>
-      <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to='/'>Return to Home</Link>
+    <div className='not_found_container'>
+      <div className='not_found_content'>
+        <h1>Sorry, the page you were looking for was not found.</h1>
+        <Link to='/' className='return_home_link'>
+          Return to Home
+        </Link>
+      </div>
     </div>
   );
 }
