48fa64e8ea7a1bc7bbd2f1ef5f976cde2a63eefe
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-24 23:21:13 +0300

    changing the layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faStar } from "@fortawesome/free-solid-svg-icons";
-
 function Home() {
   return (
-    <div className='home_page'>
-      <h2>You got the travel plans, We got the travel vans</h2>
-      <p>Add adventure to your life by joining #vanlife movement</p>
-      <p>rent the perfect van to make your perfect road trip.</p>
-      <button>Find your van</button>
+    <div className='home_page bg-gray-100 min-h-screen flex flex-col justify-center items-center'>
+      <h2 className='text-3xl font-semibold mb-4'>
+        You got the travel plans, We got the travel vans
+      </h2>
+      <p className='text-lg mb-2'>
+        Add adventure to your life by joining #vanlife movement
+      </p>
+      <p className='text-lg mb-8'>
+        Rent the perfect van to make your perfect road trip.
+      </p>
+      <button className='px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300'>
+        Find your van
+      </button>
     </div>
   );
 }
