4b06f1c0d999525423b9c0c9dc6eb415d10183a1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-02-17 23:47:28 +0300

    changing the layout

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
 import { vans } from "../../data/vans_data";
+
 function Vans() {
   const hostVan = vans.map((van) => (
-    <div key={van.id} className='host_van_card'>
-      <Link to={`${van.id}`}>
-        <div className='img'>
-          <img src={van.img} alt='' />
+    <div
+      key={van.id}
+      className='bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300'
+    >
+      <Link to={`${van.id}`} className='block'>
+        <div className='aspect-w-16 aspect-h-9'>
+          <img
+            src={van.img}
+            alt={van.name}
+            className='object-cover w-full h-full'
+          />
         </div>
-        <div className='host_van_price_name'>
-          <b>{van.name}</b>
-          <b>{van.price}</b>
+        <div className='p-4 flex justify-between items-center'>
+          <b className='text-lg font-semibold text-gray-800'>{van.name}</b>
+          <b className='text-lg font-semibold text-gray-800'>${van.price}</b>
         </div>
       </Link>
     </div>
   ));
-  return <div className='host_van_container'>{hostVan}</div>;
+
+  return (
+    <div className='container mx-auto py-12'>
+      <h2 className='text-3xl font-semibold text-center mb-8'>Our Vans</h2>
+      <div className='grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {hostVan}
+      </div>
+    </div>
+  );
 }
 
 export const HostVans = Vans;
