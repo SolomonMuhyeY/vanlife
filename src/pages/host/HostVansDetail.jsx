c3a1a339e468358fd2a5f7ef8ea3718ce81ee534
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-02-23 07:48:46 +0300

    changing the layout

--- src/pages/host/HostVansDetail.jsx
+++ src/pages/host/HostVansDetail.jsx
@@ -13,7 +13,10 @@ function HostVansDetail() {
   const hostVansEl = vans.map(
     (van) =>
       id == van.id && (
-        <div key={van.id} className='p-4 border rounded-lg shadow-lg  my-4'>
+        <div
+          key={van.id}
+          className='p-4 border rounded-lg shadow-lg my-4 bg-white'
+        >
           <div className='img_type_name_price_cont flex items-center'>
             <div className='img w-32 h-32 overflow-hidden rounded-full border-2 border-gray-300'>
               <img
@@ -25,7 +28,7 @@ function HostVansDetail() {
             <div className='ml-4'>
               {van.type ? (
                 <button
-                  className={`type_btn ${van.type} py-1 px-3 bg-blue-500 text-white rounded-lg`}
+                  className={`type_btn ${van.type} py-1 px-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white rounded-lg shadow-md hover:from-blue-700 hover:to-blue-900`}
                 >
                   {van.type}
                 </button>
@@ -43,21 +46,21 @@ function HostVansDetail() {
               style={({ isActive }) => (isActive ? activeStyle : null)}
               to={`/host/vans/${van.id}`}
               end
-              className='text-gray-700 hover:text-gray-900'
+              className='text-gray-700 hover:text-gray-900 py-2 px-4 rounded-md border border-gray-300 hover:border-gray-500 transition duration-300'
             >
               Details
             </NavLink>
             <NavLink
               style={({ isActive }) => (isActive ? activeStyle : null)}
               to={`/host/vans/${van.id}/pricing`}
-              className='text-gray-700 hover:text-gray-900'
+              className='text-gray-700 hover:text-gray-900 py-2 px-4 rounded-md border border-gray-300 hover:border-gray-500 transition duration-300'
             >
               Pricing
             </NavLink>
             <NavLink
               style={({ isActive }) => (isActive ? activeStyle : null)}
               to={`/host/vans/${van.id}/photos`}
-              className='text-gray-700 hover:text-gray-900'
+              className='text-gray-700 hover:text-gray-900 py-2 px-4 rounded-md border border-gray-300 hover:border-gray-500 transition duration-300'
             >
               Photos
             </NavLink>
@@ -66,8 +69,9 @@ function HostVansDetail() {
         </div>
       )
   );
+
   return (
-    <div className='container mb-20 mt-8 '>
+    <div className='container mb-20 mt-8'>
       <Link
         to='..'
         relative='path'
