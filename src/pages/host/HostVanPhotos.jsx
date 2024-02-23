383a9b3029a719095aa50cfa8826e9d6a9e512e7
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-02-23 07:54:38 +0300

    changing the layout

--- src/pages/host/HostVanPhotos.jsx
+++ src/pages/host/HostVanPhotos.jsx
@@ -3,8 +3,14 @@ import { useOutletContext } from "react-router-dom";
 function HostVanPhotos() {
   const { van } = useOutletContext();
   return (
-    <div style={{ margin: "2rem auto" }} className='img'>
-      <img src={van.img} alt='' />
+    <div className='flex justify-center mt-8'>
+      <div className='w-full max-w-md'>
+        <img
+          src={van.img}
+          alt={van.name}
+          className='w-full h-auto rounded-lg shadow-lg'
+        />
+      </div>
     </div>
   );
 }
