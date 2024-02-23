b4ca6e7fada6b7f92b37926a90e61246ef322411
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-02-23 09:01:02 +0300

    changing the layout

--- src/pages/host/HostVanPrice.jsx
+++ src/pages/host/HostVanPrice.jsx
@@ -2,7 +2,7 @@ import { useOutletContext } from "react-router-dom";
 
 function HostVanPrice() {
   const { van } = useOutletContext();
-  return <h3 style={{ margin: "1rem 0", textAlign: "center" }}>{van.price}</h3>;
+  return <h3>{van.price}</h3>;
 }
 
 export default HostVanPrice;
