a10a599fac54c5b24341ab2c7bc61f7bb2301505
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-02-23 07:30:41 +0300

    changing the layout

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -5,7 +5,7 @@ function Vans() {
   const hostVan = vans.map((van) => (
     <div
       key={van.id}
-      className='bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105'
+      className='rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105'
     >
       <Link to={`${van.id}`} className='block'>
         <div className='aspect-w-16 aspect-h-9'>
@@ -15,7 +15,7 @@ function Vans() {
             className='object-cover w-full h-full transition-transform duration-300 transform hover:scale-110'
           />
         </div>
-        <div className='p-4 flex justify-between items-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white'>
+        <div className='p-4 flex justify-between items-center text-white'>
           <b className='text-lg font-semibold'>{van.name}</b>
           <b className='text-lg font-semibold'>${van.price}</b>
         </div>
@@ -24,7 +24,7 @@ function Vans() {
   ));
 
   return (
-    <div className='bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 py-12'>
+    <div className='py-12'>
       <div className='container mx-auto'>
         <h2 className='text-4xl font-bold text-center mb-12 text-gray-800'>
           Discover Your Perfect Van
