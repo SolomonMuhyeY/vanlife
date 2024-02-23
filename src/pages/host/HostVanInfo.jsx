4437988ea0dee956d37a10158c72965df2e83dcb
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-02-23 07:57:21 +0300

    changing the layout

--- src/pages/host/HostVanInfo.jsx
+++ src/pages/host/HostVanInfo.jsx
@@ -3,7 +3,7 @@ import { useOutletContext } from "react-router-dom";
 function HostVanInfo() {
   const { van } = useOutletContext();
   return (
-    <div className='current_van_detail'>
+    <div className='py-4'>
       <h3>Name: {van.name}</h3>
       {van.type && <h3> Category : {van.type} </h3>}
       <p>{van.description}</p>
